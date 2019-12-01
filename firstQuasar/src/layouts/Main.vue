914a92a725b7bcd31b82b577c66973d9c0102f29
Alena Valova <a3valova@gmail.com>
2019-12-01 10:14:06 +0100

    first commit

---
+++ firstQuasar/src/layouts/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout>
+    <q-layout-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          Ahoj
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-layout-header>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+
+</style>
